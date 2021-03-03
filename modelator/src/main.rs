36067685f2a84beeb2eddaf025a9df2eb4d5ddde
Vitor Enes <vitorenesduarte@gmail.com>
2021-03-03 11:47:59 +0100

    Add command-line example

--- modelator/src/main.rs
+++ modelator/src/main.rs
@@ -9,8 +9,8 @@ async fn main() -> Result<(), modelator::Error> {
         .test("ICS03ConnectionOpenConfirmOKTest")
         .log("tlc.log");
 
+    // cargo run -- IBCTests -r test,ICS03ConnectionOpenConfirmOKTest
     let options: Options = Options::parse();
-
     let trace = modelator::run(options).await?;
     println!("{}", trace.join("\n"));
     Ok(())
