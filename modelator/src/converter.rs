809587a978adb7f700d8f937d68d5d03978d4c45
Andrey Kuprianov <andrey@informal.systems>
2021-03-16 19:00:21 +0100

    converter test (#22)

--- modelator/src/converter.rs
+++ modelator/src/converter.rs
@@ -1,5 +1,5 @@
+use std::any::{type_name, Any, TypeId};
 use std::collections::BTreeMap;
-use std::any::{Any, TypeId};
 
 pub struct Converter {
     converts: BTreeMap<(TypeId, TypeId), Box<dyn Any>>,
@@ -12,29 +12,104 @@ impl Converter {
         }
     }
 
-    pub fn add<From:  Sized + Any, To: Sized + Any>(&mut self, f: fn(&Self, From) -> To) 
-    {
+    pub fn add<From: Sized + Any, To: Sized + Any>(&mut self, f: fn(&Self, From) -> To) {
         let type_ids = (TypeId::of::<From>(), TypeId::of::<To>());
-        if !self.converts.contains_key(&type_ids) {
-            self.converts.insert(type_ids, Box::new(f));
+        self.converts.insert(type_ids, Box::new(f));
+    }
+
+    pub fn convert<From: Sized + Any, To: Sized + Any>(&self, x: From) -> To {
+        match self.get::<From, To>() {
+            Some(f) => f(x),
+            None => panic!(
+                "Undefined conversion from {:?} to {:?}",
+                type_name::<From>(),
+                type_name::<To>()
+            ),
         }
     }
 
-    pub fn convert<From: Sized + Any, To: Sized + Any>(&self, x: From) -> To
-    {
-        match self.get::<From, To>() {
-            Some(f) => f(x),
-            None => unreachable!(),
-        }
-    }    
-
-    pub fn get<From: Sized + Any, To: Sized + Any>(&self) -> Option<Box<dyn Fn(From) -> To + '_>>
-    {
+    pub fn get<From: Sized + Any, To: Sized + Any>(&self) -> Option<Box<dyn Fn(From) -> To + '_>> {
         let type_ids = (TypeId::of::<From>(), TypeId::of::<To>());
-        self.converts.get(&type_ids).and_then(|f| 
-            f.downcast_ref::<fn(&Self, From) -> To>().and_then(|f|
-                Some(Box::new(move |x: From| f(&self, x)) as Box<dyn Fn(From) -> To>)
-            )
-        )
-    }    
+        self.converts.get(&type_ids).and_then(|f| {
+            f.downcast_ref::<fn(&Self, From) -> To>()
+                .and_then(|f| Some(Box::new(move |x: From| f(&self, x)) as Box<dyn Fn(From) -> To>))
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, PartialEq)]
+    pub struct Chain {
+        name: String,
+        id: u64,
+        default_provider: Provider,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub struct Provider {
+        name: String,
+        id: u64,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub struct Block {
+        chain: Chain,
+        height: u64,
+        id: u64,
+        provider: Provider,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub struct AbstractBlock {
+        chain: String,
+        height: u64,
+        provider: String,
+    }
+
+    #[test]
+    pub fn test() {
+        let mut c = Converter::new();
+        c.add(|_, name: String| Provider { name: name, id: 0 });
+        c.add(|c, name: String| Chain {
+            name: name.clone(),
+            id: 0,
+            default_provider: c.convert(name),
+        });
+        c.add(|c, b: AbstractBlock| Block {
+            chain: c.convert(b.chain),
+            height: b.height,
+            id: 0,
+            provider: c.convert(b.provider),
+        });
+
+        let a_block = AbstractBlock {
+            chain: "chain1".to_string(),
+            height: 1,
+            provider: "provider2".to_string(),
+        };
+
+        let block: Block = c.convert(a_block);
+
+        let expected = Block {
+            chain: Chain {
+                name: "chain1".to_string(),
+                id: 0,
+                default_provider: Provider {
+                    name: "chain1".to_string(),
+                    id: 0,
+                },
+            },
+            height: 1,
+            id: 0,
+            provider: Provider {
+                name: "provider2".to_string(),
+                id: 0,
+            },
+        };
+
+        assert_eq!(block, expected);
+    }
 }
