d331ef28c4f1335698c57cd9c3b505c8e14dc494
Andrey Kuprianov <andrey@informal.systems>
2021-03-17 11:36:34 +0100

    make Converter::default to accept functions (#22)

--- modelator/src/converter.rs
+++ modelator/src/converter.rs
@@ -15,6 +15,8 @@ impl Converter {
         Self {
             converts: BTreeMap::new(),
             named_converts: BTreeMap::new(),
+
+            // TODO: get rid of those by instead redirecting default -> convert with From == ()
             defaults: BTreeMap::new(),
             named_defaults: BTreeMap::new(),
         }
@@ -33,15 +35,15 @@ impl Converter {
     }
 
     // Define default value for type T
-    pub fn def<T: Sized + Any + Clone>(&mut self, value: T) {
+    pub fn def<T: Sized + Any>(&mut self, f: fn(&Self) -> T) {
         let type_id = TypeId::of::<T>();
-        self.defaults.insert(type_id, Box::new(value));
+        self.defaults.insert(type_id, Box::new(f));
     }
 
     // Defined named default value for type T
-    pub fn def_as<T: Sized + Any + Clone>(&mut self, name: &str, value: T) {
+    pub fn def_as<T: Sized + Any>(&mut self, name: &str, f: fn(&Self) -> T) {
         let type_id = TypeId::of::<T>();
-        self.named_defaults.insert((name.to_string(),type_id), Box::new(value));
+        self.named_defaults.insert((name.to_string(),type_id), Box::new(f));
     }
 
     // Apply conversion from From into To
@@ -70,9 +72,9 @@ impl Converter {
     }
 
     // Generate default value of type T
-    pub fn default<T: Sized + Any + Clone>(&self) -> T {
+    pub fn default<T: Sized + Any>(&self) -> T {
         match self.get_default::<T>() {
-            Some(v) => v,
+            Some(f) => f(),
             None => panic!(
                 "Undefined default for {:?}",
                 type_name::<T>(),
@@ -81,9 +83,9 @@ impl Converter {
     }    
 
     // Generate named default value of type T
-    pub fn default_as<T: Sized + Any + Clone>(&self, name: &str) -> T {
+    pub fn default_as<T: Sized + Any>(&self, name: &str) -> T {
         match self.get_default_as::<T>(name) {
-            Some(v) => v,
+            Some(f) => f(),
             None => panic!(
                 "Undefined default named '{}' for {:?}",
                 name,
@@ -108,18 +110,20 @@ impl Converter {
         })
     }
 
-    pub fn get_default<T: Sized + Any + Clone>(&self) -> Option<T> {
+    pub fn get_default<T: Sized + Any>(&self) -> Option<Box<dyn Fn() -> T + '_>> {
         let type_id = TypeId::of::<T>();
-        self.defaults.get(&type_id)
-            .and_then(|f|f.downcast_ref::<T>())
-            .map(|v| v.clone())
+        self.defaults.get(&type_id).and_then(|f| {
+            f.downcast_ref::<fn(&Self) -> T>()
+                .and_then(|f| Some(Box::new(move || f(&self)) as Box<dyn Fn() -> T>))
+        })
     }
 
-    pub fn get_default_as<T: Sized + Any + Clone>(&self, name: &str) -> Option<T> {
+    pub fn get_default_as<T: Sized + Any>(&self, name: &str) -> Option<Box<dyn Fn() -> T + '_>> {
         let type_id = TypeId::of::<T>();
-        self.named_defaults.get(&(name.to_string(), type_id))
-            .and_then(|f|f.downcast_ref::<T>())
-            .map(|v| v.clone())
+        self.named_defaults.get(&(name.to_string(), type_id)).and_then(|f| {
+            f.downcast_ref::<fn(&Self) -> T>()
+                .and_then(|f| Some(Box::new(move || f(&self)) as Box<dyn Fn() -> T>))
+        })
     }
 }
 
@@ -158,17 +162,19 @@ mod tests {
     #[test]
     pub fn test() {
         let mut c = Converter::new();
-        c.def(0u64);
-        c.add(|_, name: String| Provider { name: name, id: 0 });
+        c.def_as("height", |_| 1u64);
+        c.def_as("id", |_| 0u64);
+        c.def(|c| Provider { name: "default_provider".to_string(), id: c.default_as("id") });
+        c.add(|c, name: String| Provider { name: name, id: c.default_as("id") });
         c.add(|c, name: String| Chain {
             name: name.clone(),
             id: c.default_as("id"),
-            default_provider: c.convert(name),
+            default_provider: c.default(),
         });
         c.add(|c, b: AbstractBlock| Block {
             chain: c.convert(b.chain),
             height: b.height,
-            id: c.default(),
+            id: c.default_as("id"),
             provider: c.convert(b.provider),
         });
 
@@ -185,7 +191,7 @@ mod tests {
                 name: "chain1".to_string(),
                 id: 0,
                 default_provider: Provider {
-                    name: "chain1".to_string(),
+                    name: "default_provider".to_string(),
                     id: 0,
                 },
             },
@@ -197,8 +203,6 @@ mod tests {
             },
         };
 
-        //assert_eq!(block, expected);
-
-        println!("{:?}", block)
+        assert_eq!(block, expected);
     }
 }
