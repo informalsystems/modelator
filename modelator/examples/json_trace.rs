bcf9c11e7371e6fdfe64caaa895e1e8d6c1e29cb
Vitor Enes <vitorenesduarte@gmail.com>
2021-03-18 12:12:13 +0100

    Improve json_trace.rs example (#23)

--- modelator/examples/json_trace.rs
+++ modelator/examples/json_trace.rs
@@ -3,21 +3,13 @@ use modelator::{Error, Options};
 fn main() -> Result<(), Error> {
     let options = Options::default();
     let traces = modelator::traces(
-        "../ibc-rs/modules/tests/support/model_based/IBCTests.tla",
-        "../ibc-rs/modules/tests/support/model_based/IBCTests.cfg",
+        "tests/integration/tla/NumbersAMaxBMinTest.tla",
+        "tests/integration/tla/Numbers.cfg",
         options,
     )?;
 
-    // aggregate all traces into a json array (and each trace into a json array
-    // as well)
-    let json = serde_json::Value::Array(
-        traces
-            .into_iter()
-            .map(|trace| serde_json::Value::Array(trace.into_iter().collect::<Vec<_>>()))
-            .collect::<Vec<_>>(),
-    );
-    let pretty = serde_json::to_string_pretty(&json)
-        .expect("it should be possible to pretty print json traces");
-    println!("{}", pretty);
+    traces.into_iter().for_each(|trace| {
+        println!("{}", trace);
+    });
     Ok(())
 }
