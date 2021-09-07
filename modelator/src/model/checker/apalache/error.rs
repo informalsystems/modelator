3ab0bab040a318c87defa523a202da04b5adcc5b
Daniel Tisdall <daniel@informal.systems>
2021-09-07 11:34:24 +0100

    Strips out ApalacheError, adds stderr

--- modelator/src/model/checker/apalache/error.rs
+++ modelator/src/model/checker/apalache/error.rs
@@ -9,6 +9,7 @@ use std::fmt;
 pub struct ApalacheError {
     summary: String,
     stdout: String,
+    stderr: String,
 }
 
 impl fmt::Display for ApalacheError {
@@ -17,69 +18,10 @@ impl fmt::Display for ApalacheError {
     }
 }
 
-const APALACHE_STDOUT_ERRORS: [&str; 21] = [
-    "Error by TLA+ parser",
-    "Configuration error (see the manual)",
-    "Input error (see the manual)",
-    "Assignment error",
-    "rewriter error",
-    "unexpected expression",
-    "Unexpected expressions in the specification (see the error messages)",
-    "Input error (see the manual)",
-    "Typing input error",
-    "Type checker error",
-    "Irrecoverable preprocessing error",
-    "no rule to rewrite a TLA+ expression",
-    "rewriter error",
-    "error when rewriting to SMT",
-    "type error",
-    "unexpected TLA+ expression",
-    "internal error",
-    "checker error",
-    "unexpected TLA+ expression",
-    "The specification is malformed",
-    "Unable to find assignments for all state variables",
-];
-
-/// Return the first line containing one of the errors, if any.
-/// The list of errors is not exhaustive.
-fn try_extract_error_reason(apalache_stdout: &str) -> Option<String> {
-    for line in apalache_stdout.split('\n') {
-        for summary in APALACHE_STDOUT_ERRORS {
-            if line.contains(summary) {
-                return Some(line.to_string());
-            }
-        }
-    }
-    None
-}
-
-impl ApalacheError {
-    pub(crate) fn new(apalache_stdout: &str) -> Self {
-        let summary = match try_extract_error_reason(apalache_stdout) {
-            Some(line) => line,
-            None => {
-                "(Modelator) unable to parse reason for error from Apalache output.".to_string()
-            }
-        };
-        Self {
-            summary,
-            stdout: apalache_stdout.to_string(),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_errors() {
-        let s = "0\n0error when rewriting to SMT0\n0";
-        let compare = ApalacheError {
-            summary: "0error when rewriting to SMT0".to_string(),
-            stdout: s.to_string(),
-        };
-        assert_eq!(ApalacheError::new(s), compare);
-    }
+    fn test_TODO() {}
 }
