b369309c25309aa48fe110d5dabeb70a2e8679d5
Daniel Tisdall <daniel@informal.systems>
2021-10-11 13:03:59 +0100

    Adds a TLDR comment explaining integration test practice

--- modelator/tests/integration/main.rs
+++ modelator/tests/integration/main.rs
@@ -1,5 +1,6 @@
 // We follow the approach proposed in the following link for integration tests:
 // https://matklad.github.io/2021/02/27/delete-cargo-integration-tests.html
+// TLDR: use 1 integration test in tests/integration/
 
 use modelator::artifact::JsonTrace;
 use modelator::cli::{App, CliStatus};
