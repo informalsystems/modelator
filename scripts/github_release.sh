5f4f9d1a72e212463563f7430ff71f607f0addd3
Ranadeep Biswas <mail@rnbguy.at>
2021-11-19 19:07:06 +0100

    prelim workflows and scripts

---
+++ scripts/github_release.sh
@@ -0,0 +1,9 @@
+# the `[release] vX.Y.Z` is merged to `main` now
+
+# publish to crates.io, pypi.org and go registry
+
+# create a tag `vX.Y.Z`
+
+# create a github release with change log
+
+# for now, there is not asset provided on github releases
---
+++ scripts/prepare_release.sh
@@ -0,0 +1,9 @@
+# create a new branch `[release] vX.Y.Z` with ${RELEASE_VERSION}
+
+# update the versions on Rust crate
+
+# update the version on Python module
+
+# nothing to do for Go lang for now
+
+# create the pull request from `[release] vX.Y.Z` to `main`
