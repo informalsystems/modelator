0aa7d41b16cec662915fdc34df0802082d6f5889
Ranadeep Biswas <mail@rnbguy.at>
2022-01-12 12:28:50 +0100

    clap shell completions

--- rs/modelator/src/main.rs
+++ rs/modelator/src/main.rs
@@ -2,5 +2,8 @@ use clap::Parser;
 
 pub fn main() {
     let cli_app = modelator::cli::App::parse();
-    cli_app.run().exit()
+
+    if !cli_app.try_print_completions() {
+        cli_app.run().exit()
+    }
 }
