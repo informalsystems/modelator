6d4e2c6079a992e6fe11e12c5b18195bcd1bdeb2
ivan-gavran <ivan@informal.systems>
2022-05-05 15:28:13 +0200

    factored out wrapping into mod-py style apalache command

--- python/modelator/parse.py
+++ python/modelator/parse.py
@@ -1,39 +1,23 @@
 import argparse
-import os
+
 from typing import Tuple
 from modelator_py.apalache.pure import apalache_pure
 from . import utils
 # import utils
 
-from .constants import DEFAULT_APALACHE_JAR
-# from constants import DEFAULT_APALACHE_JAR
+
 
 """
 The function sends the TLA+ model file (`tla_file_content`) to apalache parse command.
 Returns (True, "") if tla_file_content parses, otherwise (False, msg), where msg is a message for why 
 the model does not parse.
-
-TODO: cfg_file_content argument is not used. Apalache only has a command for parsing .tla files
-and in its new version it does not even load .cfg files by default. Is there a need to parse .cfg files
-or shall we take all the common arguments from .cfg and make them command-line arguments?
 """
-def parse(
-    tla_file_content: str,     
-    cfg_file_content: str = None
-    ) -> Tuple[bool, str]: 
+def parse(tla_file_content: str) -> Tuple[bool, str]: 
 
     if not isinstance(tla_file_content, str):
         raise TypeError("`tla_file_content` should be string")
     
-    json_command = {}
-    json_command["args"] = {}
-    json_command["args"]["cmd"] = "parse"    
-
-    specName = utils.extract_tla_module_name(tla_file_content) + ".tla"
-    json_command["args"]["file"] = specName
-
-    json_command["files"] = {specName: tla_file_content}    
-    json_command["jar"] = os.path.abspath(DEFAULT_APALACHE_JAR)
+    json_command = utils.wrap_apalache_command(cmd="parse", tla_file_content=tla_file_content)
 
     result = apalache_pure(json=json_command)
 
@@ -44,10 +28,11 @@ def parse(
         return (False, error)
     
 
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("model_name")
-    parser.add_argument("config_name")
+    parser.add_argument("model_name")    
     
     args = parser.parse_args()
     
--- python/modelator/utils.py
+++ python/modelator/utils.py
@@ -1,6 +1,6 @@
+import os
 import re
-
-#TODO: add tests for these methods
+from .constants import DEFAULT_APALACHE_JAR
 
 def extract_tla_module_name(tla_file_content: str):
     match = re.search("-+[ ]*MODULE[ ]*(?P<moduleName>\w+)[ ]*-+", tla_file_content)
@@ -27,3 +27,16 @@ def extract_parse_error(parser_output: str):
         return None
     else:
         return "\n".join(report)        
+
+
+def wrap_apalache_command(cmd, tla_file_content, args=None):
+    json_command = {}
+    json_command["args"] = {}
+    json_command["args"]["cmd"] = "parse"    
+
+    specName = extract_tla_module_name(tla_file_content) + ".tla"
+    json_command["args"]["file"] = specName
+
+    json_command["files"] = {specName: tla_file_content}    
+    json_command["jar"] = os.path.abspath(DEFAULT_APALACHE_JAR)
+    return json_command
