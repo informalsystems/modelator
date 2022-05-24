9e25c69f2a6f55ee0d39c751e4046cb5d21a8ad0
Ranadeep Biswas <mail@rnbguy.at>
2022-05-24 17:59:58 +0200

    ITF pretty print test

--- tests/test_itf.py
+++ tests/test_itf.py
@@ -5,7 +5,16 @@ from modelator.itf import ITF
 
 
 @pytest.mark.parametrize("itf_json_file", glob.glob("tests/traces/itf/*.itf.json"))
-def test_itf_parse(snapshot, itf_json_file):
+def test_itf_parse_and_diff(snapshot, itf_json_file):
     trace = ITF.from_itf_json(itf_json_file)
     diff = ITF.diff(trace)
     assert (trace, diff) == snapshot
+
+
+@pytest.mark.parametrize("itf_json_file", glob.glob("tests/traces/itf/*.itf.json"))
+def test_itf_diff_print(capfd, snapshot, itf_json_file):
+    trace = ITF.from_itf_json(itf_json_file)
+    ITF.print_diff(trace)
+    out, _ = capfd.readouterr()
+    print(out)
+    assert out == snapshot
