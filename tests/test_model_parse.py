80fd8b0f578ef35c867df20b32a1678106f0ce5d
hvanz <hernan.vanzetto@gmail.com>
2022-08-08 16:16:11 +0300

    Format

--- tests/test_model_parse.py
+++ tests/test_model_parse.py
@@ -8,27 +8,23 @@ from modelator.utils.model_exceptions import ModelParsingError
 
 def test_parse_success():
     file_path = "tests/sampleFiles/parse/correct/dir1/Hello_Hi.tla"
-    files_contents = {
-        os.path.basename(file_path): open(file_path).read()
-    }
+    files_contents = {os.path.basename(file_path): open(file_path).read()}
     m = Model(file_path, "Init", "Next", files_contents)
     m._parse()
     assert isinstance(m, Model)
 
+
 def test_parse_fail_indentation():
     file_path = "tests/sampleFiles/parse/flawed/dir1/HelloFlawed1.tla"
-    files_contents = {
-        os.path.basename(file_path): open(file_path).read()
-    }
+    files_contents = {os.path.basename(file_path): open(file_path).read()}
     m = Model(file_path, "Init", "Next", files_contents)
     with pytest.raises(ModelParsingError):
         m._parse()
 
+
 def test_parse_fail_extra_comma():
     file_path = "tests/sampleFiles/parse/flawed/dir2/HelloFlawed2.tla"
-    files_contents = {
-        os.path.basename(file_path): open(file_path).read()
-    }
+    files_contents = {os.path.basename(file_path): open(file_path).read()}
     m = Model(file_path, "Init", "Next", files_contents)
     with pytest.raises(ModelParsingError):
         m._parse()
--- tests/test_model_parse_file.py
+++ tests/test_model_parse_file.py
@@ -9,10 +9,12 @@ def test_parse_file_success():
     m = Model.parse_file(file_path)
     assert isinstance(m, Model)
 
+
 def test_parse_file_fail_indentation():
     file_path = "tests/sampleFiles/parse/flawed/dir1/HelloFlawed1.tla"
     with pytest.raises(ModelParsingError):
         Model.parse_file(file_path)
+
 
 def test_parse_file_fail_extra_comma():
     file_path = "tests/sampleFiles/parse/flawed/dir2/HelloFlawed2.tla"
