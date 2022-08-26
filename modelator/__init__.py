93358a5be1779423f2dae30e0a95744d1b8b6bfa
Ranadeep Biswas <mail@rnbguy.at>
2022-08-26 18:18:08 +0200

    v0.6.1

--- modelator/__init__.py
+++ modelator/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.2"
+__version__ = "0.6.1"
 
 from .cli import app
 from .Model import Model
--- tests/test_modelator.py
+++ tests/test_modelator.py
@@ -2,4 +2,4 @@ from modelator import __version__
 
 
 def test_version():
-    assert __version__ == "0.5.2"
+    assert __version__ == "0.6.1"
