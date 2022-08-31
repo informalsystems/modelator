2341f65d5884756f1445407917f7816f19028c8f
hvanz <hernan.vanzetto@gmail.com>
2022-08-31 16:11:11 +0300

    Test scripts

--- tests/cli/traces_last_generated.sh
+++ tests/cli/traces_last_generated.sh
@@ -5,18 +5,18 @@ set -eu -o pipefail
 
 [ $# -eq 0 ] && echo "Usage: $0 <PROPERTY_NAME> [<TRACES_DIR>]" && exit 1
 
-PROPERTY_NAME=$1
+PROPERTY_NAME="$1"
 TRACES_DIR="${2:-traces}"
 
 DIR=$TRACES_DIR
-[ ! -d $DIR ] && echo "Directory $DIR does not exist" && exit 1
+[ ! -d "$DIR" ] && echo "Directory $DIR does not exist" && exit 1
 
 TIMESTAMP_SUBDIR=$(ls -rt $TRACES_DIR | tail -1)
 DIR=$DIR/$TIMESTAMP_SUBDIR
-[ ! -d $DIR ] && echo "Directory $DIR does not exist" && exit 1
+[ ! -d "$DIR" ] && echo "Directory $DIR does not exist" && exit 1
 
 DIR=$DIR/$PROPERTY_NAME
-[ ! -d $DIR ] && echo "Directory $DIR does not exist" && exit 1
+[ ! -d "$DIR" ] && echo "Directory $DIR does not exist" && exit 1
 
 # last file in the directory by time
 LAST_FILE=$(ls -rt $DIR | tail -1)
--- tests/cli/traces_num_generated.sh
+++ tests/cli/traces_num_generated.sh
@@ -5,15 +5,15 @@ set -eu -o pipefail
 
 [ $# -eq 0 ] && echo "Usage: $0 <PROPERTY_NAME> [<TRACES_DIR>]" && exit 1
 
-PROPERTY_NAME=$1
+PROPERTY_NAME="$1"
 TRACES_DIR="${2:-traces}"
 
-DIR=$TRACES_DIR
-[ ! -d $DIR ] && echo "Directory $DIR does not exist" && exit 1
+DIR="$TRACES_DIR"
+[ ! -d "$DIR" ] && echo "Directory $DIR does not exist" && exit 1
 
 TIMESTAMP_SUBDIR=$(ls -rt $TRACES_DIR | tail -1)
-DIR=$DIR/$TIMESTAMP_SUBDIR/$PROPERTY_NAME
-[ ! -d $DIR ] && echo "Directory $DIR does not exist" && exit 1
+DIR="$DIR/$TIMESTAMP_SUBDIR/$PROPERTY_NAME"
+[ ! -d "$DIR" ] && echo "Directory $DIR does not exist" && exit 1
 
 # Return the number of files in the directory
 # - tail -n+2 for discarding the first file (violation.itf.json)
