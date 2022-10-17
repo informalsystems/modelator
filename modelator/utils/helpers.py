be90fecc7f573ce671df539ef6b83641dfd39b9c
Ranadeep Biswas <mail@rnbguy.at>
2022-10-17 16:31:37 +0200

    formatting

--- modelator/utils/helpers.py
+++ modelator/utils/helpers.py
@@ -1,4 +1,4 @@
-# removes the prefix `prefix` from `text`. 
+# removes the prefix `prefix` from `text`.
 # Implemented here as a helper fucntion in order to support Python 3.8
 def remove_prefix(text, prefix):
     if text.startswith(prefix):
